05f7defeb9698b645b9ecaacc83282a0c2bb5c4a
WarCrazhed <mariozamora5126@gmail.com>
2024-12-04 07:00:30 -0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/**
+ * Route:get    Consultar
+ * Route:post   Guardar
+ * Route:delete Eliminar
+ * Route:put    Actualizar
+ */
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/blog', function () {
+    // Consulta a base de datos
+    $posts = [
+        ['id' => 1, 'title' => 'PHP', 'slug' => 'php'],
+        ['id' => 2, 'title' => 'Laravel', 'slug' => 'laravel'],
+    ];
+    return view('blog', ['posts' => $posts]);
+});
+
+Route::get('/blog/{slug}', function ($slug) {
+    // Consulta a base de datos
+    $post = $slug;
+    return view('post', ['post' => $post]);
+});
