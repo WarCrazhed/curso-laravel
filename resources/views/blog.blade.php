4e8521be16c51155c533e15a19883e30bab3e4eb
Mario Zamora <zamora@Mac-mini-de-Mario.local>
2024-12-10 09:04:29 -0600

    Migraciones, Modelos, Eloquent y Relaciones

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,15 +1,17 @@
 @extends('template')
 @section('content')
-    @foreach ($posts as $post)
-        <h1>Listado</h1>
+    <h1>Listado</h1>
 
-        @foreach ($posts as $post)
-            <p>
-                {{ $post['id'] }}
-                <a href="{{ route('post', $post['slug']) }}">
-                    {{ $post['title'] }}
-                </a>
-            </p>
-        @endforeach
+    @foreach ($posts as $post)
+        <p>
+            {{ $post->id }}
+            <a href="{{ route('post', $post->slug) }}">
+                {{ $post->title }}
+            </a>
+            <br/>
+            <span>{{ $post->user->name }}</span>
+        </p>
     @endforeach
+
+    {{ $posts->links() }}
 @endsection
