8eb39b9f5f54b6a9023377a3a72fb6f2da3cebde
Mario Zamora <zamora@Mac-mini-de-Mario.local>
2024-12-11 09:30:33 -0600

    Sistema de Inicio de sesión y Panel administrativo

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,26 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route:get    Consultar
- * Route:post   Guardar
- * Route:delete Eliminar
- * Route:put    Actualizar
- */
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
- /*
-Route::get('/', [PageController::class, 'home'])->name('home');
-Route::get('/blog', [PageController::class, 'blog'])->name('blog');
-Route::get('/blog/{slug}', [PageController::class, 'post'])->name('post');
-*/
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 Route::controller(PageController::class)->group(function () {
     Route::get('/', 'home')->name('home');
     Route::get('/blog', 'blog')->name('blog');
     Route::get('/blog/{post:slug}', 'post')->name('post');
 });
+
+Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth']);
+
+require __DIR__.'/auth.php';
